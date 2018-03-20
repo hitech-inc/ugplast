42253718a3747365540bf6a71d444c32647c74b2
Arman85 <advanced315@gmail.com>
2018-03-20 11:44:50 +0600

    add container block to new page

--- resources/views/frontend/new.blade.php
+++ resources/views/frontend/new.blade.php
@@ -9,13 +9,15 @@
 			@include('frontend.partials._sidebar')
 			<div class="col-9 order-2">
 				<div class="row">
-					<h1>{{ $mynew->title }}</h1>
-					<div class="col-3">
-						<img src="/img/news/{{ $mynew->img }}" alt="" style="max-width: 100%; border-radius: 8px">
-					</div>
-					<div class="col-9">
-						{!! $mynew->body !!}
-						<a href="/all-news" class="backButton"><span class=""></span>Назад</a>
+					<div class="container">
+						<h1>{{ $mynew->title }}</h1>
+						<div class="col-3">
+							<img src="/img/news/{{ $mynew->img }}" alt="" style="max-width: 100%; border-radius: 8px">
+						</div>
+						<div class="col-9">
+							{!! $mynew->body !!}
+							<a href="/all-news" class="backButton"><span class=""></span>Назад</a>
+						</div>
 					</div>	
 				</div>
 			</div>
