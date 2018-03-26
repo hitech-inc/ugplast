e4a117ac3052d32a09904858c8d574793b5a2b5b
Arman85 <advanced315@gmail.com>
2018-03-26 14:37:08 +0600

    update mobile version

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,14 @@
+@extends('frontend.layouts.master')
+
+@section('content')
+	@include( 'frontend.partials._topMenu' )
+	<div class="container">
+		<div class="row">
+			<div class="col-12">
+				<h2 style="text-align: center; padding: 97px">
+					Страница не найдена
+				</h2>
+			</div>
+		</div>
+	</div>
+@endsection
